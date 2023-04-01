97db2a144650812e1ecd482dfbbe91234926e4cd
taoxi_honey <70771089+2869949486@users.noreply.github.com>
2023-04-01 11:02:01 +0000

    aa

---
+++ ServerPlugins/plugins/WelcomeMusic.cs
@@ -0,0 +1,318 @@
+{
+  "Main Settings": {
+    "Open when player joins": false,
+    "Open once per wipe": true,
+    "Open at tab": 1,
+    "Block movement when browsing panel": false,
+    "Enable text tags": true,
+    "Server Title": " ",
+    "Server Logo": "",
+    "Selected button color": "0.161 0.384 0.569 1",
+    "Base Command": "info",
+    "Additional Commands": {
+      "rules": 2,
+      "help": 3
+    }
+  },
+  "Text Tabs": [
+    {
+      "Name": "末日启示录",
+      "Icon": "",
+      "Font Size": 16,
+      "Font Color": "1 1 1 1",
+      "Font Outline Color": "0 0 0 1",
+      "Font Outline Thickness": "0.5",
+      "Font": "robotocondensed-bold.ttf",
+      "Text Background Image": "",
+      "Text Alignment": 3,
+      "Text Lines": [
+        [
+          "2030年，全球遭遇了一场可怕的灾难。生化危机和核战争爆发，大部分城市被摧毁，人类文明走向尽头。在这个世界中，末日幸存者需要不断地战斗和生存，以求在这个艰难的时代中生存下去。一些幸存者聚集在一起，建造了一些简陋的庇护所。他们开始探索周围的环境，收集资源和制造工具来保护自己和他们的家人。然而，新的威胁很快就出现了，他们发现了袭击者，袭击者们自称为“佣兵”，他们以劫掠玩家为生，给幸存者带来了威胁。在末日降临后的第三年，世界上已经没有了人类的痕迹，大部分的城市都已经成为了一片荒芜的废墟。然而，在某个角落中，一群人类幸存者正在努力寻找生存的道路。他们被迫建立了一个名为“前哨站”的小型聚居点，希望能够在这个荒凉的世界中建立起一个新的文明。但是，在这个荒芜的世界中，资源的匮乏和野蛮的敌人一直是他们所面临的巨大挑战。在这样的情况下，电力的稳定供应已经成为了他们的迫切需求之一。"
+        ]
+      ],
+      "Addon (plugin name)": ""
+    },
+    {
+      "Name": "突围生存",
+      "Icon": "",
+      "Font Size": 16,
+      "Font Color": "1 1 1 1",
+      "Font Outline Color": "0 0 0 1",
+      "Font Outline Thickness": "0.5",
+      "Font": "robotocondensed-bold.ttf",
+      "Text Background Image": "",
+      "Text Alignment": 3,
+      "Text Lines": [
+        [
+          "在废墟与废土之间，生存是唯一的目标。许多人聚集在一起建造了希望之城-前哨站。这个城市由一些生存者组成，他们努力恢复科技和工业，以在末世中重建人类文明。在城市中心，建造了一座大型发电厂，它为整个城市提供电力。发电厂的建造过程是艰辛的，但经过数月的努力，终于成功了。城市中的人们现在能够有光有电，这为城市的重建带来了新的希望。此外，前哨站还建有一片种植园，里面种植了各种蔬菜和水果。人们在这里努力耕种，为城市的居民提供充足的食物和营养。不过，废土中的危险也随处可见。城市的防御体系日益完善，但却无法阻止超级变异Boss的袭击。这些怪物出现得突然，拥有强大的攻击能力和耐力，往往能够摧毁城市的一角。为了更好地应对这些威胁，城市的武器装备也在不断升级和强化。工匠们在前哨站中心建造了一座工作室，为人们提供武器修理和升级服务。在这个充满危机的末世中，前哨站的居民们必须齐心协力，共同面对外界的威胁，才能在这个艰难的时代中生存下去。"
+        ]
+      ],
+      "Addon (plugin name)": ""
+    },
+    {
+      "Name": "荒野求生",
+      "Icon": "",
+      "Font Size": 16,
+      "Font Color": "1 1 1 1",
+      "Font Outline Color": "0 0 0 1",
+      "Font Outline Thickness": "0.5",
+      "Font": "robotocondensed-bold.ttf",
+      "Text Background Image": "",
+      "Text Alignment": 3,
+      "Text Lines": [
+        [
+          "随着前哨站的建设，幸存者们开始逐渐适应了这个新的生活环境，不再是孤军奋战。他们开始建立自己的社区，分工合作，努力恢复一些旧有的生活方式。其中，电力和种植成为了最为重要的两个方面。为了满足人们对电力的需求，他们组建了电力公司，并利用废弃的核反应堆制造出了大量的电力。而在前哨站的周围，他们建立了大片的农田，通过种植和养殖来解决自给自足的问题。同时，他们也开始利用废弃的工厂和机器来生产武器和装备，以保护自己免受辐射和强盗的侵袭。"
+        ]
+      ],
+      "Addon (plugin name)": ""
+    },
+    {
+      "Name": "重建文明",
+      "Icon": "",
+      "Font Size": 16,
+      "Font Color": "1 1 1 1",
+      "Font Outline Color": "0 0 0 1",
+      "Font Outline Thickness": "0.5",
+      "Font": "robotocondensed-bold.ttf",
+      "Text Background Image": "",
+      "Text Alignment": 3,
+      "Text Lines": [
+        [
+          "在生存艰难的废土上，病毒的威胁仍然存在。人们必须谨慎小心，以免被感染。一些幸存者组成了小团队，经常在禁区外进行寻找和探险。他们发现了一种新的病毒，它可以通过水源传播。为了防止病毒的传播，人们开始了水源净化计划，并建立了许多水过滤站。这些站点需要大量的电力支持，因此人们修建了一座核电站。但是，核电站的建设并不容易。由于辐射污染，建设者们必须穿着特殊的防护服进行施工，而施工过程中还遭受了佣兵的袭击。经过团队的努力，核电站终于建成，并开始供应整个地区的电力需求。"
+        ]
+      ],
+      "Addon (plugin name)": ""
+    },
+    {
+      "Name": "辐射之战",
+      "Icon": "",
+      "Font Size": 16,
+      "Font Color": "1 1 1 1",
+      "Font Outline Color": "0 0 0 1",
+      "Font Outline Thickness": "0.5",
+      "Font": "robotocondensed-bold.ttf",
+      "Text Background Image": "",
+      "Text Alignment": 3,
+      "Text Lines": [
+        [
+          "一天，一个流浪者来到了前哨站。他自称为“辐射猎人”，并向众人展示了一种叫做“放射性水晶”的珍稀物品。据他说，这种水晶可以用来强化武器和护甲，并且可以增强战斗力。随着时间的推移，前哨站开始成为一个强大的城市。他们拥有电力、水源和农田，能够自给自足。玩家们也开始在基础设施和防御方面投入更多的时间和资源，以便在未来抵御更大的危险。然而，他们的和平生活并没有持续太久。在辐射猎人的警告下，他们发现了一只超级变异的Boss正在朝他们的城市靠近。在这场生死攸关的战斗中，他们必须团结一致，使用他们的武器和放射性水晶，为了生存而战斗。"
+        ]
+      ],
+      "Addon (plugin name)": ""
+    }
+  ],
+  "Base Ui Elements": {
+    "background": {
+      "Color": "0 0 0 0.8",
+      "Material": "assets/content/ui/uibackgroundblur.mat",
+      "Image": "",
+      "Anchor Min": "0 0",
+      "Anchor Max": "1 1",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "offset_container": {
+      "Color": "0 0 0 0",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "",
+      "Anchor Min": "0.5 0.5",
+      "Anchor Max": "0.5 0.5",
+      "Offset Min": "-680 -360",
+      "Offset Max": "680 360",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "main": {
+      "Color": "0.70 0.67 0.65 0.07",
+      "Material": "assets/content/ui/uibackgroundblur-ingamemenu.mat",
+      "Image": "",
+      "Anchor Min": "0.315 0.175",
+      "Anchor Max": "0.80 0.748",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "side": {
+      "Color": "0.70 0.67 0.65 0.07",
+      "Material": "assets/content/ui/uibackgroundblur-ingamemenu.mat",
+      "Image": "",
+      "Anchor Min": "0.187 0.175",
+      "Anchor Max": "0.308 0.748",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "content": {
+      "Color": "0 0 0 0.0",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "",
+      "Anchor Min": "0.32 0.182",
+      "Anchor Max": "0.795 0.740",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.0,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "title": {
+      "Color": "0 0 0 0",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "https://rust-server.oss-cn-shanghai.aliyuncs.com/plugins/welcomepanel/wptitle.png",
+      "Anchor Min": "0.185 0.745",
+      "Anchor Max": "0.9 0.85",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 3,
+      "Text Font (not for panels)": "robotocondensed-bold.ttf"
+    },
+    "logo": {
+      "Color": "0 0 0 0",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "https://rust-server.oss-cn-shanghai.aliyuncs.com/plugins/welcomepanel/wptitle.png",
+      "Anchor Min": "0.175 0.745",
+      "Anchor Max": "0.8 0.85",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": null,
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": null
+    },
+    "close_button": {
+      "Color": "0.757 0.259 0.161 1.00",
+      "Material": "assets/content/ui/uibackgroundblur-ingamemenu.mat",
+      "Image": "",
+      "Anchor Min": "0.73 0.125",
+      "Anchor Max": "0.80 0.16",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.2,
+      "Text (not for panels)": "开始战斗",
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": "robotocondensed-bold.ttf"
+    },
+    "next_button": {
+      "Color": "1 1 1 0.30",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "https://rustplugins.net/products/welcomepanellite/1/next.png",
+      "Anchor Min": "0.953 0.013",
+      "Anchor Max": "0.995 0.06",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.0,
+      "Text (not for panels)": "",
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": "robotocondensed-bold.ttf"
+    },
+    "back_button": {
+      "Color": "1 1 1 0.30",
+      "Material": "assets/icons/iconmaterial.mat",
+      "Image": "https://rustplugins.net/products/welcomepanellite/1/back.png",
+      "Anchor Min": "0.902 0.013",
+      "Anchor Max": "0.943 0.06",
+      "Offset Min": "0 0",
+      "Offset Max": "0 0",
+      "Fade": 0.0,
+      "Text (not for panels)": "",
+      "Text Alignment (not for panels)": 4,
+      "Text Font (not for panels)": "robotocondensed-bold.ttf"
+    }
+  },
+  "Tab Buttons": [
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.700",
+      "Anchor Max": "0.3095 0.748",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.650",
+      "Anchor Max": "0.3095 0.697",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.600",
+      "Anchor Max": "0.3095 0.647",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.550",
+      "Anchor Max": "0.3095 0.597",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.500",
+      "Anchor Max": "0.3095 0.547",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.450",
+      "Anchor Max": "0.3095 0.497",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.400",
+      "Anchor Max": "0.3095 0.447",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.350",
+      "Anchor Max": "0.3095 0.397",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.300",
+      "Anchor Max": "0.3095 0.347",
+      "Material": "assets/icons/iconmaterial.mat"
+    },
+    {
+      "Color": "0 0 0 0",
+      "Text Alignment": 4,
+      "Text Font": "robotocondensed-bold.ttf",
+      "Anchor Min": "0.1845 0.250",
+      "Anchor Max": "0.3095 0.297",
+      "Material": "assets/icons/iconmaterial.mat"
+    }
+  ]
+}
